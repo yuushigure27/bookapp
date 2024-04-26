2410a46a55c121af794524fd72aaac0669a086b8
yuushigure27 <yuushigure1227@gmail.com>
2024-04-26 14:33:52 +0000

    homes controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,17 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+  
   def index
   end
 
